8ae7379744ab97ac4d2112ec4574e62ad660de68
Răzvan Filea <razvan.filea@gmail.com>
2025-08-17 19:02:24 +0300

    Redesign the Upload/Move/Rename screens

--- app/src/main/java/net/theluckycoder/familyphotos/ui/composables/UploadComposables.kt
+++ app/src/main/java/net/theluckycoder/familyphotos/ui/composables/UploadComposables.kt
@@ -1,46 +1,44 @@
 package net.theluckycoder.familyphotos.ui.composables
 
-import androidx.compose.foundation.clickable
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.aspectRatio
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.imePadding
+import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SegmentedButton
 import androidx.compose.material3.SegmentedButtonDefaults
 import androidx.compose.material3.SingleChoiceSegmentedButtonRow
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.Immutable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.pluralStringResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import net.theluckycoder.familyphotos.R
 import net.theluckycoder.familyphotos.data.model.db.NetworkFolder
 import net.theluckycoder.familyphotos.data.model.db.Photo
 import net.theluckycoder.familyphotos.data.model.db.isPublic
-import net.theluckycoder.familyphotos.data.model.db.isPublic
 import net.theluckycoder.familyphotos.ui.LocalNavBackStack
-import net.theluckycoder.familyphotos.utils.normalize
 
 @Immutable
 enum class UploadChoice(internal val stringRes: Int) {
@@ -55,65 +53,56 @@ private fun UploadDialogContent(
     onChoiceChange: (UploadChoice) -> Unit,
     folderName: String,
     onFolderNameChange: (String) -> Unit,
-    foldersList: List<String>
-) = Column {
-    LazyRow(Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.spacedBy(0.5.dp)) {
-        items(photosToShowcase) { photo ->
-            CoilPhoto(
-                photo = photo,
-                modifier = Modifier.size(72.dp),
-                preview = true,
-                contentScale = ContentScale.Crop,
-            )
-        }
-    }
-
-    SingleChoiceSegmentedButtonRow(
-        Modifier
-            .fillMaxWidth()
-            .padding(4.dp)
-    ) {
-        UploadChoice.entries.forEachIndexed { index, choice ->
-            SegmentedButton(
-                modifier = Modifier
-                    .weight(1f)
-                    .padding(4.dp),
-                shape = SegmentedButtonDefaults.itemShape(
-                    index = index,
-                    count = UploadChoice.entries.size
-                ),
-                selected = selectedChoice == choice,
-                onClick = { onChoiceChange(choice) }
-            ) {
-                Text(stringResource(choice.stringRes))
-            }
-        }
-    }
-
-    FolderFilterTextField(folderName, onFolderNameChange)
-
-    LazyColumn(Modifier.fillMaxSize()) {
-        items(foldersList) {
-            Column {
-                Text(
-                    text = it,
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .clickable { onFolderNameChange(it) }
-                        .padding(12.dp)
+    foldersList: List<NetworkFolder>
+) = FoldersGridList(
+    folders = foldersList,
+    onFolderClick = { onFolderNameChange(it.name) },
+    folderDetailsText = { folder ->
+        pluralStringResource(R.plurals.items_photos, folder.count, folder.count)
+    },
+    folderNameFilter = folderName,
+    onSearch = onFolderNameChange,
+    extraHeader = {
+        LazyRow(Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.spacedBy(0.5.dp)) {
+            items(photosToShowcase) { photo ->
+                CoilPhoto(
+                    photo = photo,
+                    modifier = Modifier.size(72.dp),
+                    preview = true,
+                    contentScale = ContentScale.Crop,
                 )
-
-                HorizontalDivider()
             }
         }
-    }
-}
 
-@OptIn(ExperimentalMaterial3Api::class)
+        SingleChoiceSegmentedButtonRow(
+            Modifier
+                .fillMaxWidth()
+                .padding(4.dp)
+        ) {
+            UploadChoice.entries.forEachIndexed { index, choice ->
+                SegmentedButton(
+                    modifier = Modifier
+                        .weight(1f)
+                        .padding(4.dp),
+                    shape = SegmentedButtonDefaults.itemShape(
+                        index = index,
+                        count = UploadChoice.entries.size
+                    ),
+                    selected = selectedChoice == choice,
+                    onClick = { onChoiceChange(choice) }
+                ) {
+                    Text(stringResource(choice.stringRes))
+                }
+            }
+        }
+
+    }
+)
+
 @Composable
 fun UploadPhotosLayout(
     networkFolders: List<NetworkFolder>,
-    title: String,
+    actionName: String,
     photosToShowcase: List<Photo>,
     doneAction: (choice: UploadChoice, folderName: String) -> Unit,
 ) {
@@ -123,38 +112,36 @@ fun UploadPhotosLayout(
     var folderName by remember { mutableStateOf("") }
 
     Scaffold(
-        topBar = {
-            TopAppBar(
-                navigationIcon = {
-                    IconButton(onClick = { backStack.removeLastOrNull() }) {
-                        Icon(painterResource(R.drawable.ic_close), contentDescription = null)
-                    }
-                },
-                title = { Text(title) },
-                actions = {
-                    IconButton(onClick = {
-                        doneAction(choice, folderName)
-                    }) {
-                        Icon(
-                            painterResource(R.drawable.ic_action_done),
-                            tint = Color.White,
-                            contentDescription = null
-                        )
-                    }
-                },
-            )
-        },
+        bottomBar = {
+            Row(
+                Modifier
+                    .fillMaxWidth()
+                    .background(MaterialTheme.colorScheme.surfaceContainer)
+                    .navigationBarsPadding()
+                    .imePadding()
+                    .padding(16.dp),
+                horizontalArrangement = Arrangement.spacedBy(16.dp, Alignment.End)
+            ) {
+                TextButton(onClick = { backStack.removeLastOrNull() }) {
+                    Text(stringResource(R.string.action_cancel))
+                }
+
+                Button(onClick = { doneAction(choice, folderName) }) {
+                    Icon(
+                        painterResource(R.drawable.ic_action_done),
+                        contentDescription = actionName
+                    )
+
+                    Text(actionName)
+                }
+            }
+        }
     ) { contentPadding ->
-        val filteredFoldersList = remember(networkFolders, choice, folderName) {
-            val normalizedName = folderName.normalize()
-            networkFolders.asSequence()
-                .filter { it.isPublic == (choice == UploadChoice.Public) }
-                .map { it.name }
-                .filter { it.normalize().contains(normalizedName, ignoreCase = true) }
-                .toList()
+        val filteredFoldersList = remember(networkFolders, choice) {
+            networkFolders.filter { it.isPublic == (choice == UploadChoice.Public) }
         }
 
-        Box(Modifier.padding(contentPadding)) {
+        Box(Modifier.padding(bottom = contentPadding.calculateBottomPadding())) {
             UploadDialogContent(
                 photosToShowcase = photosToShowcase,
                 selectedChoice = choice,
--- app/src/main/java/net/theluckycoder/familyphotos/ui/screen/MovePhotosScreen.kt
+++ app/src/main/java/net/theluckycoder/familyphotos/ui/screen/MovePhotosScreen.kt
@@ -16,7 +16,6 @@ import net.theluckycoder.familyphotos.ui.LocalNavBackStack
 import net.theluckycoder.familyphotos.ui.LocalSnackbarHostState
 import net.theluckycoder.familyphotos.ui.composables.UploadChoice
 import net.theluckycoder.familyphotos.ui.composables.UploadPhotosLayout
-import net.theluckycoder.familyphotos.ui.viewmodel.MainViewModel
 import net.theluckycoder.familyphotos.ui.viewmodel.UploadPhotosViewModel
 
 @Composable
@@ -37,7 +36,7 @@ fun MovePhotosScreen(photoIds: LongArray) {
 
     UploadPhotosLayout(
         networkFolders = networkFolders,
-        title = stringResource(R.string.action_move_photos),
+        actionName = stringResource(R.string.action_move_photos),
         photosToShowcase = photosToShowcase.value,
         doneAction = { choice, folderName ->
             uploadPhotosViewModel.viewModelScope.launch {
