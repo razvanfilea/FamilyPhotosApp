3d26339ac0b8eed2c7b6bd4ea7b46603efa657b0
Răzvan Filea <razvan.filea@gmail.com>
2025-08-17 20:52:44 +0300

    Add the ThumbHash decoder

---
+++ app/src/main/java/net/theluckycoder/familyphotos/utils/ScaledBitmapPainter.kt
@@ -0,0 +1,42 @@
+package net.theluckycoder.familyphotos.utils
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.graphics.asImageBitmap
+import androidx.compose.ui.graphics.drawscope.DrawScope
+import androidx.compose.ui.graphics.drawscope.scale
+import androidx.compose.ui.graphics.drawscope.translate
+import androidx.compose.ui.graphics.painter.Painter
+import kotlin.io.encoding.Base64
+
+@Composable
+fun rememberThumbHashPainter(thumbHash: String): Painter = remember(thumbHash) {
+    val hash = Base64.decode(thumbHash)
+    val bitmap = ThumbHash.thumbHashToRGBA(hash).asImageBitmap()
+    ScaledBitmapPainter(bitmap)
+}
+
+class ScaledBitmapPainter(private val imageBitmap: ImageBitmap) : Painter() {
+
+    override val intrinsicSize: Size = Size.Unspecified
+
+    override fun DrawScope.onDraw() {
+        val srcWidth = imageBitmap.width.toFloat()
+        val srcHeight = imageBitmap.height.toFloat()
+        val dstWidth = size.width
+        val dstHeight = size.height
+
+        val scale: Float = maxOf(dstWidth / srcWidth, dstHeight / srcHeight)
+        val dx = (dstWidth - srcWidth * scale) / 2f
+        val dy = (dstHeight - srcHeight * scale) / 2f
+
+        translate(left = dx, top = dy) {
+            scale(scale = scale, pivot = androidx.compose.ui.geometry.Offset.Zero) {
+                drawImage(imageBitmap)
+            }
+        }
+    }
+}
+
