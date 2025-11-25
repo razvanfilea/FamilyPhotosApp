363a05d3cc89f68a8f6c56c5e7ba3555241ad82a
Răzvan Filea <razvan.filea@gmail.com>
2025-11-25 21:13:41 +0200

    Add a ThumbHashCache, improve PhotosList performance, update dependencies

--- app/src/main/java/net/theluckycoder/familyphotos/ui/viewmodel/TimelineViewModel.kt
+++ app/src/main/java/net/theluckycoder/familyphotos/ui/viewmodel/TimelineViewModel.kt
@@ -5,25 +5,38 @@ import androidx.compose.foundation.lazy.grid.LazyGridState
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import androidx.paging.Pager
+import androidx.paging.PagingData
 import androidx.paging.cachedIn
 import androidx.paging.filter
+import androidx.paging.insertSeparators
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.flatMapLatest
-import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.flowOn
+import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
+import kotlinx.datetime.LocalDateTime
+import kotlinx.datetime.TimeZone
+import kotlinx.datetime.number
+import kotlinx.datetime.toJavaMonth
+import kotlinx.datetime.toLocalDateTime
 import net.theluckycoder.familyphotos.data.local.datastore.SettingsDataStore
-import net.theluckycoder.familyphotos.data.local.datastore.UserDataStore
 import net.theluckycoder.familyphotos.data.model.PhotoType
+import net.theluckycoder.familyphotos.data.model.db.Photo
 import net.theluckycoder.familyphotos.data.model.db.isPublic
 import net.theluckycoder.familyphotos.data.repository.PhotosRepository
 import net.theluckycoder.familyphotos.ui.viewmodel.MainViewModel.Companion.PAGING_CONFIG
+import java.time.format.TextStyle
+import java.util.Locale
 import javax.inject.Inject
+import kotlin.time.Clock
+import kotlin.time.ExperimentalTime
+import kotlin.time.Instant
 
 @OptIn(ExperimentalCoroutinesApi::class)
 @HiltViewModel
@@ -64,5 +77,49 @@ class TimelineViewModel @Inject constructor(
         viewModelScope.launch(Dispatchers.Default) {
             settingsStore.setSelectedPhotoType(photoType)
         }
+    }
+
+    companion object {
+        private fun Flow<PagingData<Photo>>.mapPagingPhotos() = map { pagingData ->
+            pagingData
+                .insertSeparators { before, after ->
+                    after ?: return@insertSeparators null
+
+                    computeSeparatorText(before, after)
+                }
+        }
+
+        @OptIn(ExperimentalTime::class)
+        fun computeSeparatorText(before: Photo?, after: Photo): String? {
+            val timeZone = TimeZone.currentSystemDefault()
+            val beforeDate = before?.let {
+                val instant = Instant.fromEpochSeconds(it.timeCreated)
+                instant.toLocalDateTime(timeZone)
+            }
+            val afterDate =
+                Instant.fromEpochSeconds(after.timeCreated)
+                    .toLocalDateTime(timeZone)
+
+            if (beforeDate != null && beforeDate.month.number == afterDate.month.number && beforeDate.year == afterDate.year)
+                return null
+
+            val currentDate =
+                Clock.System.now().toLocalDateTime(TimeZone.currentSystemDefault())
+            return buildDateString(currentDate, afterDate)
+        }
+
+        private fun buildDateString(currentDate: LocalDateTime, afterDate: LocalDateTime) =
+            buildString {
+                append(
+                    afterDate.month.toJavaMonth().getDisplayName(
+                        TextStyle.FULL,
+                        Locale.forLanguageTag("ro-RO")
+                    )
+                )
+
+                val year = afterDate.year
+                if (year != currentDate.year)
+                    append(' ').append(year)
+            }.replaceFirstChar { if (it.isLowerCase()) it.titlecase(Locale.getDefault()) else it.toString() }
     }
 }
--- app/src/main/java/net/theluckycoder/familyphotos/utils/ScaledBitmapPainter.kt
+++ app/src/main/java/net/theluckycoder/familyphotos/utils/ScaledBitmapPainter.kt
@@ -1,44 +1,11 @@
 package net.theluckycoder.familyphotos.utils
 
-import android.graphics.Bitmap
-import android.util.Log
 import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.ImageBitmap
-import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.graphics.drawscope.DrawScope
 import androidx.compose.ui.graphics.drawscope.scale
 import androidx.compose.ui.graphics.drawscope.translate
 import androidx.compose.ui.graphics.painter.Painter
-import kotlinx.coroutines.CancellationException
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.ensureActive
-import kotlinx.coroutines.withContext
-import kotlin.io.encoding.Base64
-
-fun loadThumbHashImage(thumbHash: String): Bitmap? =
-    try {
-        val hash = Base64.decode(thumbHash)
-        ThumbHash.thumbHashToRGBA(hash)
-    } catch (e: Exception) {
-        Log.e("ThumbHash", "Failed to decode thumb hash", e)
-        null
-    }
-
-suspend fun loadThumbHashPainter(thumbHash: String): Painter? =
-    withContext(Dispatchers.Default) {
-        try {
-            val hash = Base64.decode(thumbHash)
-            ensureActive()
-            val bitmap = ThumbHash.thumbHashToRGBA(hash).asImageBitmap()
-            ensureActive()
-            ScaledBitmapPainter(bitmap)
-        } catch (e: CancellationException) {
-            throw e
-        } catch (e: Exception) {
-            Log.e("ThumbHash", "Failed to decode thumb hash", e)
-            null
-        }
-    }
 
 class ScaledBitmapPainter(private val imageBitmap: ImageBitmap) : Painter() {
 
