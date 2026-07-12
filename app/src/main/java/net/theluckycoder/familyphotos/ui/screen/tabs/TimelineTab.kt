4928e7ac3c6a98ca06374a3965cdd17944e9c8fe
Răzvan Filea <razvan.filea@gmail.com>
2026-07-12 15:08:11 +0300

    Move hardcoded UI strings to strings.xm

--- app/src/main/java/net/theluckycoder/familyphotos/ui/screen/tabs/TimelineTab.kt
+++ app/src/main/java/net/theluckycoder/familyphotos/ui/screen/tabs/TimelineTab.kt
@@ -35,6 +35,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.pluralStringResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -176,7 +177,7 @@ private fun FailedConnectionCard(mainViewModel: MainViewModel) = Card(
         )
 
         Text(
-            "Failed to connect to server",
+            stringResource(R.string.error_server_connection),
             fontSize = 18.sp,
             fontWeight = FontWeight.SemiBold,
         )
